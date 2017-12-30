fb686c49631d0c421a5d99563567659af936a627
alpaycelik <alpay.celik@bil.omu.edu.tr>
2017-12-30 21:34:34 +0200

    Media player ses çalma, durdurma eklendi.

--- app/src/main/java/celik/alpay/muzikapp/MainActivity.java
+++ app/src/main/java/celik/alpay/muzikapp/MainActivity.java
@@ -1,7 +1,10 @@
 package celik.alpay.muzikapp;
 
+import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        Button b1 = (Button) findViewById(R.id.button);
+        final MediaPlayer mediaPlayer = MediaPlayer.create(this, R.raw.istiklal);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.start();
+            }
+        });
+        Button b2 = (Button) findViewById(R.id.button2);
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mediaPlayer.pause();
+            }
+        });
+ }
 }
